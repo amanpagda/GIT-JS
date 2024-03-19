d89e3341692b1974b4ba0b0b34a8033339d5e4a2
AMAN PAGDA <alishpagda08@gmail.com>
2024-03-19 12:32:27 +0530

    modifie

--- p2.js
+++ p2.js
@@ -1,5 +1,13 @@
-myFunction();
-
-function myFunction() {
-    
+const person1 = {
+    fullName : function() {
+        return this.fname + " " + this.lname; 
+    }
 }
+
+const person2 = {
+    fname : "aman",
+    lname : "pagda", 
+}
+
+let x = person1.fullName.call(person2);
+document.querySelector("#p2").innerHTML = x;
