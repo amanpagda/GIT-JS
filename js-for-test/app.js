c302ddd7525873d3a14a9946b64f26f9831eb940
AMAN PAGDA <alishpagda08@gmail.com>
2024-03-22 12:12:45 +0530

    dark-white

--- js-for-test/app.js
+++ js-for-test/app.js
@@ -1,18 +1,12 @@
-const button = document.querySelectorAll('.buttons');
+const i = document.querySelector('#click');
 const body = document.querySelector('body');
-const white = document.querySelector('#white');
-const dark = document.querySelector('#black');
 
-button.forEach(function(mnb){
-    mnb.addEventListener('click', function(a){
-        if(a.target.id === 'white'){
-            body.style.backgroundColor = 'white';
-            if (white == '') {
-                white.body.display = 'none'
-            }
-        } else if(a.target.id === 'black') {
-            body.style.backgroundColor = 'rgb(24, 26, 27)';
-           
-        }
-    })
+i.addEventListener('click', function aman(e) {
+    if(e.target.id === 'click'){
+        body.style.backgroundColor = "#212121";
+        body.style.color = "white";
+    } else {
+        body.style.backgroundColor = "#fff";
+        body.style.color = "black";
+    }
 })
